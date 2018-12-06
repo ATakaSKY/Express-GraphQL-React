39054ef625e7eadd727657cd76e1c44267756edd
Thakur <aakthakur@deloitte.com>
2018-12-06 22:55:19 +0530

    spaceX launcher

---
+++ public/precache-manifest.6ab1bb0d0e6c6d480b1b6ba12ddbfb82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2978a3a3b7d061fbb3a5",
+    "url": "/static/js/main.2978a3a3.chunk.js"
+  },
+  {
+    "revision": "0ada1cfef8204c445aa6",
+    "url": "/static/js/1.0ada1cfe.chunk.js"
+  },
+  {
+    "revision": "2978a3a3b7d061fbb3a5",
+    "url": "/static/css/main.e005e63c.chunk.css"
+  },
+  {
+    "revision": "237b32739d7abd8daad9c87f8efc7da1",
+    "url": "/index.html"
+  }
+];
