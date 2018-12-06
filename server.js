f3d434eda17673bce1495476f273780e5ef7abbd
Thakur <aakthakur@deloitte.com>
2018-12-06 23:09:15 +0530

    corrected port in server.js

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 })
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
